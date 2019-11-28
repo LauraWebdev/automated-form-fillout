1235e777e1d817b43432f6018ac4400f8b9d1315
Andreas Heimann <andreas@iklikla.de>
2019-11-28 23:36:32 +0100

    finished

--- bot.js
+++ bot.js
@@ -1,12 +1,13 @@
 const puppeteer = require('puppeteer');
 
 // Settings
-let settingsHeadless = false;
+let settingsHeadless = true;
 
 // Text Sources
 const textSalutations = require('./texts/salutations.json');
 const textIntroductions = require('./texts/introductions.json');
-const textReason = require('./texts/reasons.json');
+const textOrganizations = require('./texts/organisations.json');
+const textFindings = require('./texts/findings.json');
 const textNames = require('./texts/names.json');
 const textLastnames = require('./texts/lastnames.json');
 const textMailhosts = require('./texts/mailhosts.json');
@@ -19,20 +20,22 @@ const textEndingSalutations = require('./texts/endingsalutations.json');
 
 	let randomSalutation = textSalutations[Math.floor(Math.random()*textSalutations.length)];
 	let randomIntroduction = textIntroductions[Math.floor(Math.random()*textIntroductions.length)];
-	let randomReason = textReason[Math.floor(Math.random()*textReason.length)];
+	let randomOrganization = textOrganizations[Math.floor(Math.random()*textOrganizations.length)];
+	let randomFinding = textFindings[Math.floor(Math.random()*textFindings.length)];
 	let randomName = textNames[Math.floor(Math.random()*textNames.length)];
 	let randomLastname = textLastnames[Math.floor(Math.random()*textLastnames.length)];
 	let randomMailhost = textMailhosts[Math.floor(Math.random()*textMailhosts.length)];
 
-	let constructedMail = randomName + "." + randomLastname + "@" + randomMailhost;
+	let constructedMail = makeMailSave(randomName + "." + randomLastname + "@" + randomMailhost);
 
 	randomIntroduction = randomIntroduction.replace("{NAME}", randomName + " " + randomLastname);
-	randomIntroduction = randomIntroduction.replace("{REASON}", randomReason);
+	randomIntroduction = randomIntroduction.replace("{ORG}", randomOrganization);
+	randomIntroduction = randomIntroduction.replace("{FINDING}", randomFinding);
 
 	let randomEnding = textEndings[Math.floor(Math.random()*textEndings.length)];
 	let randomEndingsalutation = textEndingSalutations[Math.floor(Math.random()*textEndingSalutations.length)];
 
-	let outputMessage = randomSalutation + ", \n" + randomIntroduction + "\n\n" + "\n\n" + randomEnding + "\n\n" + randomEndingsalutation + "\n" + randomName + " " + randomLastname;
+	let outputMessage = randomSalutation + ", \n" + randomIntroduction + "\n\n" + randomEnding + "\n\n" + randomEndingsalutation + "\n" + randomName + " " + randomLastname;
 
 	console.log("Opening Browser");
 
@@ -71,10 +74,16 @@ const textEndingSalutations = require('./texts/endingsalutations.json');
 	console.log("Sending Form");
 
 	// Submit the form.
-	await page.evaluate(() => document.querySelector('.et_pb_contact_submit').click());
+	await page.evaluate(() => document.querySelector('input[type="submit"]').click());
 
-	console.log("Done, next one!");
+	setTimeout(function() {
+		console.log("Done, next one!");
 
-	// Keep the browser open.
-	// browser.close();
+		// Keep the browser open.
+		browser.close();
+	}, 500);
 })();
+
+function makeMailSave(email) {
+	return email.replace("ß", "ss").replace(" ", "").replace("ö", "oe").replace("ä", "ae").replace("ü", "ue").replace("Ö", "Oe").replace("Ä", "Ae").replace("Ü", "Ue");
+}
